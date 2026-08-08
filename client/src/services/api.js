8680b5c1fa5e5c94e0c781e0e0ca08ff1dd2f946
sparkstack230 <sakthisiva8486@gmail.com>
2026-08-08 15:51:06 +0530

    update api url to production server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://learning-management-system-hsqp.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL
